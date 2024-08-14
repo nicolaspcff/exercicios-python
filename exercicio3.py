97a1ca9689a1cda6fc2cda4ea4b318fee7a0d882
nicolinhak7 <161538082+nicolaspcff@users.noreply.github.com>
2024-08-13 21:05:40 -0300

    kwdfgu
    ewtyh

---
+++ exercicio3.py
@@ -0,0 +1,9 @@
+num1 = int(input("digite o 1° número:"))
+num2 = int(input("digite o 2° número:"))
+num3 = int(input("digite o 3° número:"))
+if (num1>num2 and num1>3):
+    print ("O primeiro número é o maior")
+elif (num2>num1 and num2>num3):
+    print ("O segundo numero é maior")
+else:
+    print ("O terceiro numero é maior")
---
+++ exercicio4.py
@@ -0,0 +1,9 @@
+num1= int(input("Digite o primeio número"))
+operador= (input("D9igite o operador"))
+num2= int(input("Digite o segundo numero"))
+if (operador=='+'):
+    resultado = num1+num2
+    print (resultado)
+else:
+    resultado = num1-num2
+    print("resultado")
